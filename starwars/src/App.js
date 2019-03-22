048f4eec3c2f13dcda8d3ffdf3040c9bc22897a4
Adetunji <ashennaike@gmail.com>
2019-03-22 11:58:16 -0400

    went in a failed direction

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -29,10 +29,43 @@ class App extends Component {
       });
   };
 
+  makeCharacterCard = info => {
+    // let characterCard 
+    let characterCard = this.state.starwarsChars.map(card => {
+      return (
+        info.birth_year = card.birth_year,
+        info.created = card.created,
+        info.edited = card.edited,
+        info.eye_color = card.eye_color,
+        info.films = card.films,
+        info.gender = card.gender,
+        info.hair_color = card.hair_color,
+        info.height = card.height,
+        info.homeworld = card.homeworld,
+        info.mass = card.mass,
+        info.name = card.name,
+        info.skin_color = card.skin_color,
+        info.species = card.species,
+        info.startships = card.startships,
+        info.url = card.url,
+        info.vehicles = card.vehicles
+      )
+    })
+    console.log(characterCard)
+    
+  };
+
+
+
+
   render() {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className='character-container'>
+        {console.log(this.state.starwarsChars)}
+          <p>{this.characterCard}</p>
+        </div>
       </div>
     );
   }
